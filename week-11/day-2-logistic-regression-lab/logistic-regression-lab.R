006a92c1b6b8023c671fb0063decaef1bc75fd17
Miles Drake <44380377+miles-drake@users.noreply.github.com>
2020-05-06 09:04:32 +0100

    Separate plot exporting into a new script

--- week-11/day-2-logistic-regression-lab/logistic-regression-lab.R
+++ week-11/day-2-logistic-regression-lab/logistic-regression-lab.R
@@ -141,38 +141,3 @@ auc <- list(
 )
 
 # tenure is likely to be the best predictor, with the highest auc value of 0.74
-
-# Export Plots ------------------------------------------------------------
-
-plots_to_export <- c(
-  "plot_churn_rate_contract",
-  "plot_churn_rate_dependents",
-  "plot_churn_rate_gender",
-  "plot_churn_rate_internet_service",
-  "plot_churn_rate_partner",
-  "plot_churn_rate_phone_service",
-  "plot_churn_rate_senior_citizen",
-  "plot_roc_curves"
-)
-
-if(export_plots){
-  
-  for(element in plots_to_export){
-    
-    file_name <- element %>% 
-      str_c(".png") %>% 
-      str_to_lower() %>% 
-      str_replace_all("_", "-")
-    
-    ggsave(
-      filename = file_name,
-      plot = eval(parse(text = element)),
-      device = "png",
-      path = "plots"
-    )
-    
-  }
-  
-  rm(element, file_name)
-  
-}
